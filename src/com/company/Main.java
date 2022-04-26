44374fd26de0315b259e76e68d2cb6fc1042d165
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-26 23:53:03 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str = "Aarush singh";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='i'){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
+
